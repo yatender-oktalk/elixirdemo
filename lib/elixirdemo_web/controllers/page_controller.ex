127ab34fe2d70096d25dd898eb5fd8e7b45f9596
Yatender <yatender@getvokal.com>
2019-03-27 11:00:26 +0530

    adding documentation

--- lib/elixirdemo_web/controllers/page_controller.ex
+++ lib/elixirdemo_web/controllers/page_controller.ex
@@ -1,16 +1,34 @@
 defmodule ElixirdemoWeb.PageController do
+  @moduledoc """
+  This module generally handles all requests related to the page
+  """
+
   use ElixirdemoWeb, :controller
 
+  @doc """
+  This method will accept the connection and given params,
+  call the sum method of n natural numbers
+  and will display on the web page
+  """
   def index(conn, params) do
     contents = sum(params["num"] |> convert_to_int())
     render conn, "index.html", contents: contents, num: params["num"]
   end
 
+  @doc """
+  This method works in the tail recursion manner
+  accumulator will get added to the number and each iteration we'll reduce the number
+  once number is equal to zero we will return the accumulator back.
+  simple tail recursion is being used here
+  """
   def sum(num), do: sum(0, num)
   def sum(acc, 0), do: acc
   def sum(acc, num), do: sum(acc + num, num - 1)
 
-
+  @doc """
+  This method converts to integer
+  accepted types are decimal, string, float
+  """
   def convert_to_int(value) do
     case is_binary(value) do
       false ->
